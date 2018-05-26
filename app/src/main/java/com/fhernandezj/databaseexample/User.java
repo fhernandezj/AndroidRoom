496981af9163c6637cab5b5695d829dbe179e5d9
Francisco Hernandez <fhernandezj@uninorte.edu.co>
2018-05-26 13:32:51 -0500

    User model created

--- app/src/main/java/com/fhernandezj/databaseexample/User.java
+++ app/src/main/java/com/fhernandezj/databaseexample/User.java
@@ -1,8 +1,63 @@
 package com.fhernandezj.databaseexample;
 
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
 /**
  * Created by fhernandez on 26/05/2018.
  */
 
-class User {
+@Entity
+public class User {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @ColumnInfo(name = "first_name")
+    String firstName;
+
+    @ColumnInfo(name = "last_name")
+    String lastName;
+
+    @ColumnInfo(name = "email")
+    String email;
+
+    public User(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
