8310cc2765ca9963a15235385dc142ea62039cde
Francisco Hernandez <fhernandezj@uninorte.edu.co>
2018-05-26 13:12:18 -0500

    Created 'CreateUser' activity

--- app/src/main/java/com/fhernandezj/databaseexample/MainActivity.java
+++ app/src/main/java/com/fhernandezj/databaseexample/MainActivity.java
@@ -1,13 +1,33 @@
 package com.fhernandezj.databaseexample;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton fab;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        fab = findViewById(R.id.fab);
+        recyclerView = findViewById(R.id.recycler_view);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, CreateUserActivity.class));
+            }
+        });
     }
 }
